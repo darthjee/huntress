b5b9d68d1e99e3c04a4401a0a2dd8059e276760b
darthjee <darthjee@gmail.com>
2022-09-25 17:17:13 -0300

    Add volume

--- huntress/source/app/server.js
+++ huntress/source/app/server.js
@@ -3,8 +3,4 @@ const requestHandler = require('./request_handler');
 
 const server = http.createServer(requestHandler)
 
-console.info('here', requestHandler);
-
 server.listen(80)
-
-console.info('there');
