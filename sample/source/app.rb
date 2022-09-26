5b3b72aff4d068e0b9ed75fd007598959e6faf99
darthjee <darthjee@gmail.com>
2022-09-26 12:47:29 -0300

    Use delegator correctrly

--- sample/source/app.rb
+++ sample/source/app.rb
@@ -2,12 +2,11 @@ require 'sinatra'
 require 'yaml'
 require 'sinclair'
 
+require_relative './app/application'
 require_relative './app/routes_config'
 require_relative './app/route'
 
 set :port, 80
 set :bind, '0.0.0.0'
 
-config = RoutesConfig.load_file('config/routes.yml')
-
-config.routes.each(&:apply)
+Application.start
--- sample/source/app/route.rb
+++ sample/source/app/route.rb
@@ -2,6 +2,7 @@ class Route < Sinclair::Options
   with_options :path, :content
 
   def apply
+    content = self.content
     Sinatra::Delegator.target.get(path) do
       content
     end
