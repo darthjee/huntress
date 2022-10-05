4b256742a004546c6684a2eac2f4fb0180593948
Fernando Favini <darthjee@gmail.com>
2022-10-05 13:45:34 -0300

    Checks trims (#6)

--- source/app/request_handler.js
+++ source/app/request_handler.js
@@ -2,36 +2,36 @@ const http = require('http');
 const _ = require('lodash');
 
 class RequestHandler {
-  constructor(request, response) {
+  constructor (request, response) {
     this.request = request;
     this.response = response;
 
     _.bindAll(this, '_handleResponse', '_handleData', '_end');
   }
 
-  call() {
+  call () {
     http.get(this._url(), this._handleResponse);
   }
 
-  _url() {
-    return 'http://sample/'  + this.request.url;
+  _url () {
+    return 'http://sample/' + this.request.url;
   }
 
-  _handleResponse(response) {
+  _handleResponse (response) {
     response
       .on('data', this._handleData)
       .on('end', this._end);
   }
 
-  _handleData(data) {
+  _handleData (data) {
     this.response.write(data);
   }
 
-  _end() {
+  _end () {
     this.response.end();
   }
 }
 
 module.exports = (request, response) => {
   return new RequestHandler(request, response).call();
-}
+};
--- source/app/server.js
+++ source/app/server.js
@@ -2,9 +2,9 @@ const http = require('http');
 const requestHandler = require('./request_handler');
 
 const server = http.createServer(requestHandler);
-const port = 80
+const port = 80;
 
 server.listen(port);
-console.info('------------------------------------------------')
-console.info('listening at port '  + port);
-console.info('------------------------------------------------')
+console.info('------------------------------------------------');
+console.info('listening at port ' + port);
+console.info('------------------------------------------------');
