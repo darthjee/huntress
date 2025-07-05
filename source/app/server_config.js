f76e3dda626a5d5ba51273a21ec060889eea4806
darthjee <darthjee@gmail.com>
2025-07-04 22:06:34 -0300

    Add server config

---
+++ source/app/server_config.js
@@ -0,0 +1,9 @@
+class ServerConfig {
+  constructor(domain, port, basePath) {
+    this.domain = domain;
+    this.port = port;
+    this.basePath = basePath;
+  }
+}
+
+module.exports = ServerConfig;
