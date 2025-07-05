8a230c74222e32f32829ba4e69b33834371d79b4
darthjee <darthjee@gmail.com>
2025-07-04 23:25:44 -0300

    Refactor server

--- source/app/server.js
+++ source/app/server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const requestHandler = require('./request_handler');
 
 const RemoteConfig = require('./remote_config');
-const server = http.createServer(requestHandler.build(new RemoteConfig({domain: 'sample', port: 80, basePath: '/'})));
+const remoteConfig = new RemoteConfig({domain: 'sample', port: 80, basePath: '/'});
+const server = http.createServer(requestHandler.build(remoteConfig));
 const port = 80;
 
 server.listen(port);
