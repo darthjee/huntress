80873a599a7b291b0d11b27fe2e5d7cf399f9101
darthjee <darthjee@gmail.com>
2025-07-04 22:51:08 -0300

    Refactor config

--- source/app/server_config.js
+++ source/app/server_config.js
@@ -10,15 +10,12 @@ class ServerConfig {
 
   fullUrl(path) {
     const url = this._createUrl(path);
-    if ((this.protocol === 'http' && this.port !== 80) || (this.protocol === 'https' && this.port !== 443)) {
-      url.port = this.port;
-    }
     return url.toString();
   }
 
   _createUrl(path) {
     const fullPath = this.basePath + path.replace(/^\//, '');
-    return new URL(fullPath, `${this.protocol}://${this.domain}`);
+    return new URL(fullPath, `${this.protocol}://${this.domain}:${this.port}`);
   }
 }
 
